1da1342b5a95b5616d4dab29feddb43d1011d0ec
LeeGilk <leeamazon888@gmail.com>
2024-12-14 14:58:21 -0800

    menu stuff

--- Scripts/control.gd
+++ Scripts/control.gd
@@ -3,8 +3,9 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	$Main_menu/Play.pressed.connect(self._on_play_pressed)
+	$Main_menu/Options.pressed.connect(self._on_options_pressed)
+	$Main_menu/Quit.pressed.connect(self._on_quit_pressed)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
